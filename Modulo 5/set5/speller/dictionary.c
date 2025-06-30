11529efc4be79c4d309ea1476bd6e4c5ec4f132c
Grasielly R <159390727+grasyzip@users.noreply.github.com>
2025-06-30 15:02:09 -0300

    Modulo 5: Estrutura de Dados

---
+++ Modulo 5/set5/speller/dictionary.c
@@ -0,0 +1,98 @@
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+
+#include "dictionary.h"
+#define N 100000
+
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+}
+node;
+node *table[N];
+
+unsigned int word_count = 0;
+
+unsigned int hash(const char *word)
+{
+    unsigned long hash = 5381;
+    int c;
+
+    while ((c = tolower(*word++)))
+    {
+        hash = ((hash << 5) + hash) + c; // hash * 33 + c
+    }
+
+    return hash % N;
+}
+
+bool load(const char *dictionary)
+{
+    FILE *file = fopen(dictionary, "r");
+    if (file == NULL)
+    {
+        return false;
+    }
+    char word[LENGTH + 1];
+
+    while (fscanf(file, "%s", word) != EOF)
+    {
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            fclose(file);
+            return false;
+        }
+
+        strcpy(n->word, word);
+        unsigned int index = hash(word);
+
+        n->next = table[index];
+        table[index] = n;
+
+        word_count++;
+    }
+
+    fclose(file);
+    return true;
+}
+
+unsigned int size(void)
+{
+    return word_count;
+}
+
+bool check(const char *word)
+{
+    unsigned int index = hash(word);
+
+    for (node *cursor = table[index]; cursor != NULL; cursor = cursor->next)
+    {
+        if (strcasecmp(cursor->word, word) == 0)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool unload(void)
+{
+    for (int i = 0; i < N; i++)
+    {
+        while (table[i] != NULL)
+        {
+            node *tmp = table[i]->next;
+            free(table[i]);
+            table[i] = tmp;
+        }
+    }
+
+    return true;
+}
